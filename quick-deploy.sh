168142e180be08549e85bdda443f9f45db14ee4a
Chris Bawden <35166625+chrisjbawden@users.noreply.github.com>
2023-12-30 19:24:53 +1000

    Update quick-deploy.sh

--- quick-deploy.sh
+++ quick-deploy.sh
@@ -2,28 +2,26 @@
 
 
 # Check if the OS is Ubuntu 22 or higher
-if [[ $(lsb_release -si) != "Ubuntu" || $(lsb_release -rs | cut -d. -f1) -lt 22 ]]; then
-  echo "This script requires Ubuntu 22 or higher."
+if [[$(lsb_release -si) != "Ubuntu" ]]; then
+echo ""
+  echo "This script was created for Ubuntu"
 fi
 
 # Confirm before continuing
-read -p "This script is intended for Ubuntu 22 or higher. Do you want to continue? (y/n): " confirm
+read -p "This script is intended for Ubuntu - Do you want to continue? (y/n): " confirm
 if [[ $confirm != [Yy] ]]; then
+echo ""
   echo "Script aborted."
   sleep 5
   exit
 fi
 
-# Define the target directory
-target_directory="/usr/share/cockpit/portainer"
-parent_directory="/usr/share/cockpit"
-
 # Check if the upgrade option is provided
 if [[ "$1" == "upgrade" ]]; then
   # Check if the target directory exists
-  if [ -d "$target_directory" ]; then
+  if [ -d "/usr/share/cockpit/portainer" ]; then
     # Delete existing directory
-    rm -rf "$target_directory" || echo "Failed to delete existing directory." ; sleep 5 ; exit
+    rm -rf "/usr/share/cockpit/portainer" || echo "Failed to delete existing directory." ; sleep 5 ; exit
   fi
 fi
 
@@ -33,17 +31,20 @@ if [ -d "$target_directory" ]; then
   mv "$target_directory" "$target_directory.old" || echo "Failed to rename existing directory." ; sleep 5 ; exit
 fi
 
-# Create the target directory
-#mkdir -p "$target_directory" || error_exit "Failed to create target directory."
-
 # Download the .tar file
-tar_url="https://github.com/chrisjbawden/cockpit-portainer-application/releases/download/v1.1-beta/cockpit-portainer-application-1.1-beta.tar"  # Replace with the actual URL
-wget "$tar_url" -O /tmp/portainer-temp.tar || error_exit "Failed to download the tar file."
+tar_url=""  # Replace with the actual URL
+wget https://github.com/chrisjbawden/cockpit-portainer-application/releases/download/v1.1-beta/cockpit-portainer-application-1.1-beta.tar
 
 # Extract the .tar file to the target directory
-tar -xf /tmp/portainer-temp.tar -C "$parent_directory" --strip-components=1 || error_exit "Failed to extract the tar file."
+tar -xf cockpit-portainer-application-1.1-beta.tar
+
+sudo mv portainer /usr/share/cockpit
 
 # Cleanup: Remove the downloaded .tar file
-rm /tmp/portainer-temp.tar || error_exit "Failed to remove temporary tar file."
+rm cockpit-portainer-application-1.1-beta.tar
 
+echo ""
+echo ""
 echo "Script executed successfully."
+echo ""
+echo ""
